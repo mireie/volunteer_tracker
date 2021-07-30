6e75d644997a913d248437b9c9fc23c369e97067
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-07-30 11:40:04 -0700

    pass volunteer detail integration test

--- app.rb
+++ app.rb
@@ -11,12 +11,19 @@ DB = PG.connect({ dbname: "volunteer_tracker" })
 
 get ('/') do
   @projects = Project.all
+  @volunteers = Volunteer.all
   erb(:index)
 end
 
 get ('/project/:id') do
   @project = Project.find(params[:id].to_i)
+  @volunteers = @project.volunteers
   erb(:project)
+end
+
+get ('/volunteer/:id') do
+  @volunteer = Volunteer.find(params[:id].to_i)
+  erb(:volunteer)
 end
 
 get ('/project/:id/edit') do
@@ -30,10 +37,22 @@ post ('/project/new') do
   redirect to ("/")
 end
 
+post ('/volunteer/project/:id') do
+  @volunteer = Volunteer.new(:name => params[:name], :project_id => params[:id])
+  @volunteer.save
+  redirect to ("/")
+end
+
 patch ('/project/:id/edit') do
   @project = Project.find(params[:id].to_i)
   @project.update(params)
   redirect to ("/")
+end
+
+patch ('/volunteer/:id/edit') do
+  @volunteer = Volunteer.find(params[:id].to_i)
+  @volunteer.update(params)
+  redirect to ("volunteer/#{params[:id]}")
 end
 
 delete ('/project/:id') do
