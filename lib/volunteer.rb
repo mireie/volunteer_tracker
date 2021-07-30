304a68a1f894c9041e957755269eeaf69dd2197a
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-07-30 15:58:45 -0700

    fix 'voulnteer', update style

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -68,7 +68,7 @@ class Volunteer
 
   def delete
     DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
-    # DB.exec("DELETE FROM voulenteers WHERE volunteer_id = #{@id};")
+    DB.exec("DELETE FROM projects WHERE volunteer_id = #{@id};")
   end
 
   def total_hours
