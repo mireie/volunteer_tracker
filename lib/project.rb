c6e378788c650251950fcc2dd08a345869b1f026
Michael Reiersgaard <michael.reiersgaard@gmail.com>
2021-07-30 09:15:20 -0700

    pass update test

--- lib/project.rb
+++ lib/project.rb
@@ -45,8 +45,10 @@ class Project
     end
   end
 
-  # def update()
-  # end
+  def update(params)
+    @title = params.fetch(:title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
 
   # def delete
   # end
